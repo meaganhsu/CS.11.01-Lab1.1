1ac34eba43a20b5a01ba5df13d46f3a35eb22b6b
meaganhsu <myth72@student.cis.edu.hk>
2023-09-08 12:37:08 +0800

    Commit

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
-
     public static void main(String[] args) {
-        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+
+        int age = userInput.nextInt();
+        int datingAge = 7 + (age / 2);
+
+        System.out.println("\n" + age + "-year olds should date somebody who is at least " + datingAge + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
-
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter your age: ");
 
+        int age = userInput.nextInt();
+        int pageNumber = 100 - age;
+
+        System.out.println("\n" + age + "-year olds should read at least " + pageNumber + " pages before giving up on a book.");
     }
 }
